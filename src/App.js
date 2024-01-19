1e1343a484f79de9955f15c7671f433dae780260
chomusuke9072 <chomusuke9072@gmail.com>
2024-01-19 15:21:23 +0900

    uuid 변경 및 삭제기능 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
+import uuid from "react-uuid";
 import "./App.css";
 
 function App() {
   const [todo, setTodo] = useState([
     {
-      id: 1,
+      id: uuid(),
       title: "리액트 공부하기",
       contents: "리액트 기초를 공부해봅시다.",
       isDone: false,
     },
     {
-      id: 2,
+      id: uuid(),
       title: "리액트를 공부합시다.",
       contents: "리액트 노션 보기!!!",
       isDone: true,
@@ -31,7 +32,7 @@ function App() {
 
   const clickAddButtonHandler = () => {
     const newTodo = {
-      id: todo.length + 1,
+      id: uuid(),
       title,
       contents,
       isDone: false,
@@ -39,13 +40,6 @@ function App() {
 
     setTodo([...todo, newTodo]);
   };
-
-  const todoList = todo.filter(function (todo) {
-    return todo.isDone === false;
-  });
-  const doneList = todo.filter(function (todo) {
-    return todo.isDone === true;
-  });
 
   return (
     <div>
@@ -71,34 +65,66 @@ function App() {
         <h2>할 일 목록!</h2>
       </div>
       <div className="app-style">
-        {todoList.map(function (item) {
-          return (
-            <div key={item.id} className="component-style">
-              <h2>{item.title}</h2>
-              <p>{item.contents}</p>
-              <div className="gapButton">
-                <button className="newButton">삭제하기</button>
-                <button className="newButton">완료</button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        <h2>완료 목록!!</h2>
-        <div className="app-style">
-          {doneList.map(function (item) {
+        {todo
+          .filter((todo) => todo.isDone === false)
+          .map(function (item) {
             return (
               <div key={item.id} className="component-style">
-                <h2>{item.title}</h2>
+                <h2>
+                  {item.title}
+                  {item.id}
+                </h2>
                 <p>{item.contents}</p>
                 <div className="gapButton">
-                  <button className="newButton">삭제하기</button>
-                  <button className="newButton">취소</button>
+                  <button
+                    className="newButton"
+                    onClick={() => {
+                      const delTodo = todo.filter((todo) => {
+                        return todo.id !== item.id;
+                      });
+
+                      setTodo(delTodo);
+                    }}
+                  >
+                    삭제하기
+                  </button>
+                  <button className="newButton">완료</button>
                 </div>
               </div>
             );
           })}
+      </div>
+      <div>
+        <h2>완료 목록!!</h2>
+        <div className="app-style">
+          {todo
+            .filter((todo) => todo.isDone === true)
+            .map(function (item) {
+              return (
+                <div key={item.id} className="component-style">
+                  <h2>
+                    {item.title}
+                    {item.id}
+                  </h2>
+                  <p>{item.contents}</p>
+                  <div className="gapButton">
+                    <button
+                      className="newButton"
+                      onClick={() => {
+                        const delTodo = todo.filter((todo) => {
+                          return todo.id !== item.id;
+                        });
+
+                        setTodo(delTodo);
+                      }}
+                    >
+                      삭제하기
+                    </button>
+                    <button className="newButton">취소</button>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
